798789b484cdae9b20c479d87cca33ff4775ccab
Patrick Roza <roza@sixnetworks.net>
2017-08-16 21:56:07 +0200

    Add postcss cssnext and import

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -207,6 +207,8 @@ module.exports = {
                   ident: 'postcss',
                   plugins: () => [
                     require('postcss-flexbugs-fixes'),
+                    require('postcss-import'),
+                    require('postcss-cssnext')(),
                     autoprefixer({
                       browsers: [
                         '>1%',
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -216,6 +216,8 @@ module.exports = {
                   ident: 'postcss',
                   plugins: () => [
                     require('postcss-flexbugs-fixes'),
+                    require('postcss-import'),
+                    require('postcss-cssnext')(),
                     autoprefixer({
                       browsers: [
                         '>1%',
