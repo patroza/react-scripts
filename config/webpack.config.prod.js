628a16dfeb1941efda8c4afd52b8e19c821e0406
Patrick Roza <roza@sixnetworks.net>
2017-08-16 22:03:02 +0200

    Autoprefixer already included

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -218,15 +218,6 @@ module.exports = {
                     require('postcss-flexbugs-fixes'),
                     require('postcss-import'),
                     require('postcss-cssnext')(),
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009',
-                    }),
                   ],
                 },
               },
